3a6c8f4ee1868b4018663dbd26b2e0a28ada5f2d
Nathan Cook <cookna1@appstate.edu>
2018-09-06 12:07:00 -0400

    count input stream counts characters in input

--- src/headfirst/designpatterns/decorator/io/countInputStream.java
+++ src/headfirst/designpatterns/decorator/io/countInputStream.java
@@ -1,7 +1,24 @@
-public class countLetterInputStream extends FilterInputStream {
+import java.io.*;
+
+public class countInputStream extends FilterInputStream {
     int count = 0;
 
-    public 
+    public countInputStream(InputStream in) {
+        super(in);
+    }
 
+    public int read() throws IOException {
+        int c = in.read();
+        if (c != 1)
+            count++;
+        return c; 
 
+    }
+
+    public int read(byte[] b, int offset, int len) throws IOException {
+        int result = in.read(b, offset, len);
+        count += result;
+        return result; 
+
+    }
 }
